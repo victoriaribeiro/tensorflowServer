c51fa6760d9efb050f4a022737d150e200f22ce1
victoria <victoria@localhost.localdomain>
2018-09-02 20:25:25 -0300

    vgg 16 versão intermediária

--- dataTraining.py
+++ dataTraining.py
@@ -13,23 +13,58 @@ import itertools
 def main():
     train_path = '/home/victoria/Downloads/Food-11/training'
     valid_path = '/home/victoria/Downloads/Food-11/validation'
-    train_batches = ImageDataGenerator().flow_from_directory(train_path, target_size=(255,255),  
+    train_batches = ImageDataGenerator(rescale=1./223).flow_from_directory(train_path, target_size=(224,224),  
     classes=['bread','dairy','dessert' , 'egg'  ,'fried',  'meat',  'pasta',  'rice',  'seafood',  'soup',  'vegetables'
 ], batch_size=20 )
 
-    valid_batches = ImageDataGenerator().flow_from_directory(valid_path, target_size=(255,255),  
+    valid_batches = ImageDataGenerator(rescale=1./223).flow_from_directory(valid_path, target_size=(224,224),  
         classes=['bread','dairy','dessert' , 'egg'  ,'fried',  'meat',  'pasta',  'rice',  'seafood',  'soup',  'vegetables'
     ], batch_size=20 )
 
-    model = Sequential([
-        Conv2D(32,(3,3), activation='relu', input_shape=(255,255,3)),
-        Flatten(),
-        Dense(11,activation='softmax'),
-    ])
+    # model = Sequential([
+    #     Conv2D(32,(3,3), activation='relu', input_shape=(224,224,3)),
+    #     Flatten(),
+    #     Dense(11,activation='softmax'),
+    # ])
+
+    vgg16_model = keras.applications.vgg16.VGG16()
+    # vgg16_model = keras.application.vgg16.VGG16()
+    # vgg16_model.summary()
+
+    type(vgg16_model)
+
+    # 
+
+    model = Sequential()
+    i=0;
+    for layer in vgg16_model.layers:
+        i+=1
+        if(i<=22):
+            model.add(layer)
+
+    model.summary()
+    model.layers.pop()
+    # model.layers.pop()
+    # model.layers.pop()
+    # model.add(Dense(11, activation="softmax"))
+
+    # model.outputs = [model.layers[-1].output]
+    # model.layers[-1].outbound_nodes = []
+
+    for layer in model.layers:
+        layer.trainable = False
+
+    model.summary()
+
+    model.add(Dense(11, activation="softmax"))
+    model.summary()
+
 
     model.compile(Adam(lr=.0001), loss='categorical_crossentropy', metrics=['accuracy'])
 
-    model.fit_generator(train_batches, steps_per_epoch=4, validation_data=valid_batches, validation_steps=4,
-    epochs=5,verbose=2)
+    model.fit_generator(train_batches, steps_per_epoch=480, validation_data=valid_batches, validation_steps=480, epochs=5,verbose=2)
+
+    model.save_weights('pesos.h5')
+
 
 main()
