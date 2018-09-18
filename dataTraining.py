a4bfb49a773b559e0f476276db0f37b212d433cf
victoria <victoriaribeiro@github.com.br>
2018-09-18 18:06:42 -0300

    mudanças para rodar no Pedro

--- dataTraining.py
+++ dataTraining.py
@@ -7,19 +7,23 @@ from keras.optimizers import Adam
 from keras.metrics import categorical_crossentropy
 from keras.layers.normalization import BatchNormalization
 from keras.layers.convolutional import *
+   
 import itertools
 
 
 def main():
-    train_path = '/home/victoria/Downloads/Food-11/training'
-    valid_path = '/home/victoria/Downloads/Food-11/validation'
+    train_path = 'food-11/training'
+    valid_path = 'food-11/validation'
+
+    #train_path = Path("C:/Users/victo/OneDrive/Documentos/TCC/Food-11/training")
+    #valid_path = Path("C:/Users/victo/OneDrive/Documentos/TCC/Food-11/validation")
     train_batches = ImageDataGenerator(rescale=1./223).flow_from_directory(train_path, target_size=(224,224),  
     classes=['bread','dairy','dessert' , 'egg'  ,'fried',  'meat',  'pasta',  'rice',  'seafood',  'soup',  'vegetables'
-], batch_size=20 )
+], batch_size=50 )
 
     valid_batches = ImageDataGenerator(rescale=1./223).flow_from_directory(valid_path, target_size=(224,224),  
         classes=['bread','dairy','dessert' , 'egg'  ,'fried',  'meat',  'pasta',  'rice',  'seafood',  'soup',  'vegetables'
-    ], batch_size=20 )
+    ], batch_size=50 )
 
     # model = Sequential([
     #     Conv2D(32,(3,3), activation='relu', input_shape=(224,224,3)),
@@ -62,7 +66,7 @@ def main():
 
     model.compile(Adam(lr=.0001), loss='categorical_crossentropy', metrics=['accuracy'])
 
-    model.fit_generator(train_batches, steps_per_epoch=480, validation_data=valid_batches, validation_steps=480, epochs=5,verbose=2)
+    model.fit_generator(train_batches, steps_per_epoch=200, validation_data=valid_batches, validation_steps=100, epochs=50,verbose=2)
 
     model.save_weights('pesos.h5')
 
